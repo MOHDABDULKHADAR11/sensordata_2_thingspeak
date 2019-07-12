0012d0692ad8fb437e0d576f0571d3be58b8c15f
MOHDABDULKHADAR11 <52824397+MOHDABDULKHADAR11@users.noreply.github.com>
2019-07-12 21:32:23 +0530

    Add files via upload

---
+++ TUNIOT__15156.ino
@@ -0,0 +1,50 @@
+
+/////////////////////////////////
+// Generated with a lot of love//
+// with TUNIOT FOR ESP8266     //
+// Website: Easycoding.tn      //
+/////////////////////////////////
+#include <ESP8266WiFi.h>
+
+#include <ESP8266HTTPClient.h>
+
+WiFiClient client;
+
+String thingSpeakAddress= "http://api.thingspeak.com/update?";
+String writeAPIKey;
+String tsfield1Name;
+String request_string;
+
+HTTPClient http;
+
+void setup()
+{
+    WiFi.disconnect();
+   WiFi.begin("varhakhan","1234567890");
+  while ((!(WiFi.status() == WL_CONNECTED))){
+    delay(3000);
+
+  }
+
+}
+
+
+void loop()
+{
+
+    if (client.connect("api.thingspeak.com",80)) {
+      request_string = thingSpeakAddress;
+      request_string += "key=";
+      request_string += "QV0T5DU962UU3MM3";
+      request_string += "&";
+      request_string += "field1";
+      request_string += "=";
+      request_string += analogRead(A0);
+      http.begin(request_string);
+      http.GET();
+      http.end();
+
+    }
+    delay(4000);
+
+}
